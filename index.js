8f1f022b9f5867f71fafb4ec785d6ddd82c53947
Shing Lyu <Shing.Lyu@dazn.com>
2019-04-12 11:40:16 +0200

    Render button works

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-const title="test title"
-document.getElementById('form').innerHTML = render_form();
-document.getElementById('output').innerHTML = render_output();
+function updateVariables() {
+  var inputs = document.getElementsByTagName('input');
+  console.log(inputs)
+  for (input of Array.from(inputs)){
+    window[input.id] = input.value;
+  }
+}
+window.addEventListener('load', function() {
+  document.getElementById('form').innerHTML = render_form();
+  updateVariables();
+  document.getElementById('output').innerHTML = render_output();
+
+  document.getElementById('render_btn').addEventListener('click', function(){
+    updateVariables();
+    document.getElementById('output').innerHTML = render_output();
+  });
+})
